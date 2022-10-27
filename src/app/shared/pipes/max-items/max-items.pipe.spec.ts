b9afaf4d34c994c604f1bb04f5a57975a4432f4e
Clara Castillo <claracastillochale@gmail.com>
2022-10-27 19:10:19 +0200

    test: Remove Karma

--- src/app/shared/pipes/max-items/max-items.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { MaxItemsPipe } from './max-items.pipe';
-
-describe('MaxItemsPipe', () => {
-  it('create an instance', () => {
-    const pipe = new MaxItemsPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
