e1533e06930913c844500b9ebe63d69217bca08b
Clara Castillo <claracastillochale@gmail.com>
2022-10-27 18:39:53 +0200

    chore: remove unused components

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,13 +2,12 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { TopAnimesComponent } from '@shared-components/top-animes/top-animes.component';
-import { AnimeGenresComponent } from '@shared-components/anime-genres/anime-genres.component';
 import { RouterLinkWithHref } from '@angular/router';
 
 @Component({
   selector: 'plast-home',
   standalone: true,
-  imports: [CommonModule, MatButtonModule, TopAnimesComponent, AnimeGenresComponent, RouterLinkWithHref],
+  imports: [CommonModule, MatButtonModule, TopAnimesComponent, RouterLinkWithHref],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
--- src/app/shared/pipes/is-home-active/is-home-active.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { IsHomeActivePipe } from './is-home-active.pipe';
-
-describe('IsHomeActivePipe', () => {
-  it('create an instance', () => {
-    const pipe = new IsHomeActivePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
