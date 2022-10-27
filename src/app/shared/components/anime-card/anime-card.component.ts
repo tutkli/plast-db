08dd78db272c11ccdd64d7d66c368951a28d573a
Clara Castillo <claracastillochale@gmail.com>
2022-10-27 18:29:17 +0200

    refactor(HomeComponent): Update layout and add responsiveness.

--- src/app/shared/components/anime-card/anime-card.component.ts
+++ src/app/shared/components/anime-card/anime-card.component.ts
@@ -1,18 +1,16 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Anime } from '../../../core/models/anime/anime.model';
-import { FlexModule } from '@angular/flex-layout';
 import { MaxItemsPipe } from '@pipes/max-items/max-items.pipe';
-import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'plast-anime-card',
   standalone: true,
-  imports: [CommonModule, FlexModule, MaxItemsPipe, MatIconModule],
+  imports: [CommonModule, MaxItemsPipe],
   templateUrl: './anime-card.component.html',
   styleUrls: ['./anime-card.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AnimeCardComponent {
-  @Input() anime!: Anime;
+  @Input() anime: Anime | undefined;
 }
--- src/app/shared/components/top-animes/top-animes.component.ts
+++ src/app/shared/components/top-animes/top-animes.component.ts
@@ -3,20 +3,20 @@ import { CommonModule } from '@angular/common';
 import { Observable } from 'rxjs';
 import { Anime } from '../../../core/models/anime/anime.model';
 import { JikanService } from '@services/jikan/jikan.service';
-import { FlexModule } from '@angular/flex-layout';
 import { AnimeCardComponent } from '../anime-card/anime-card.component';
 import { MaxItemsPipe } from '@pipes/max-items/max-items.pipe';
 
 @Component({
   selector: 'plast-top-animes',
   standalone: true,
-  imports: [CommonModule, FlexModule, AnimeCardComponent, MaxItemsPipe],
+  imports: [CommonModule, AnimeCardComponent, MaxItemsPipe],
   templateUrl: './top-animes.component.html',
   styleUrls: ['./top-animes.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TopAnimesComponent implements OnInit {
   topAnimes$!: Observable<Anime[]>;
+  placeHolderAnimes: number[] = new Array(5);
 
   constructor(private jikanService: JikanService) {}
 
