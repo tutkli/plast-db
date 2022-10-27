1893ec09b136226cc99a19ba744cbf4d755409a0
Clara Castillo <claracastillochale@gmail.com>
2022-10-27 16:36:51 +0200

    refactor(Home): Remove FlexLayoutModule from HomeComponent

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,5 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FlexModule } from '@angular/flex-layout';
 import { MatButtonModule } from '@angular/material/button';
 import { TopAnimesComponent } from '@shared-components/top-animes/top-animes.component';
 import { AnimeGenresComponent } from '@shared-components/anime-genres/anime-genres.component';
@@ -9,7 +8,7 @@ import { RouterLinkWithHref } from '@angular/router';
 @Component({
   selector: 'plast-home',
   standalone: true,
-  imports: [CommonModule, FlexModule, MatButtonModule, TopAnimesComponent, AnimeGenresComponent, RouterLinkWithHref],
+  imports: [CommonModule, MatButtonModule, TopAnimesComponent, AnimeGenresComponent, RouterLinkWithHref],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
