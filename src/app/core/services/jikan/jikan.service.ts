3d2652d7d14be6cfe19fdbb59eca6ece4beee90e
Clara Castillo <claracastillochale@gmail.com>
2022-11-26 20:53:08 +0100

    build: update dependencies

--- src/app/core/services/jikan/jikan.service.ts
+++ src/app/core/services/jikan/jikan.service.ts
@@ -3,39 +3,41 @@ import { from, map, Observable } from 'rxjs';
 import {
   Anime,
   AnimeCharacter,
+  AnimeClient,
   AnimeSearchParams,
-  JikanClient,
   JikanResponse,
   JikanUniqueResponse,
   TopAnimeFilter,
+  TopClient,
 } from '@tutkli/jikan-ts';
 
 @Injectable({
   providedIn: 'root',
 })
 export class JikanService {
-  private readonly jikanClient = new JikanClient();
+  private readonly topClient = new TopClient({ loggerOptions: { enabled: true } });
+  private readonly animeClient = new AnimeClient();
 
   getTopAnimes(): Observable<Anime[]> {
-    return from(this.jikanClient.top.getTopAnime({ filter: TopAnimeFilter.airing })).pipe(
+    return from(this.topClient.getTopAnime({ filter: TopAnimeFilter.airing })).pipe(
       map((jikanResponse: JikanResponse<Anime>) => jikanResponse.data)
     );
   }
 
   getAnimeSearch(searchParams?: AnimeSearchParams): Observable<Anime[]> {
-    return from(this.jikanClient.anime.getAnimeSearch(searchParams)).pipe(
+    return from(this.animeClient.getAnimeSearch(searchParams)).pipe(
       map((jikanResponse: JikanResponse<Anime>) => jikanResponse.data)
     );
   }
 
   getAnimeDetail(malId: number): Observable<Anime> {
-    return from(this.jikanClient.anime.getAnimeById(malId)).pipe(
+    return from(this.animeClient.getAnimeById(malId)).pipe(
       map((jikanResponse: JikanUniqueResponse<Anime>) => jikanResponse.data)
     );
   }
 
   getAnimeCharacters(malId: number): Observable<AnimeCharacter[]> {
-    return from(this.jikanClient.anime.getAnimeCharacters(malId)).pipe(
+    return from(this.animeClient.getAnimeCharacters(malId)).pipe(
       map((jikanResponse: JikanResponse<AnimeCharacter>) => jikanResponse.data)
     );
   }
