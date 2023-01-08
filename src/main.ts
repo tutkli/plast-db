e615ba1367760ef4e9aa026e60ff2bdec830af76
Clara Castillo <claracastillochale@gmail.com>
2023-01-08 12:30:15 +0100

    build: remove unnecesary services

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,10 @@
-import { APP_INITIALIZER, enableProdMode, importProvidersFrom } from '@angular/core';
-import { environment } from './environments/environment';
+import { importProvidersFrom } from '@angular/core';
 import { bootstrapApplication, BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideHttpClient } from '@angular/common/http';
 import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import { APP_ROUTES } from '@routes/routes.const';
-import { InitService } from '@services/init/init.service';
 import { provideSvgIcons } from '@ngneat/svg-icon';
 import { menuIcon } from '@app/svg/menu';
 import { closeIcon } from '@app/svg/close';
@@ -14,29 +12,12 @@ import { searchIcon } from '@app/svg/search';
 import { chevronLeftIcon } from '@app/svg/chevron-left';
 import { chevronRightIcon } from '@app/svg/chevron-right';
 
-if (environment.production) {
-  enableProdMode();
-}
-
-export function initializeAppFactory(initService: InitService) {
-  return (): void => {
-    initService.initializeApp().then();
-  };
-}
-
 bootstrapApplication(AppComponent, {
   providers: [
     importProvidersFrom(BrowserModule),
-    importProvidersFrom(BrowserAnimationsModule),
-    importProvidersFrom(HttpClientModule),
+    provideAnimations(),
+    provideHttpClient(),
     provideRouter(APP_ROUTES, withInMemoryScrolling({ scrollPositionRestoration: 'enabled' })),
     provideSvgIcons([menuIcon, closeIcon, searchIcon, chevronLeftIcon, chevronRightIcon]),
-    InitService,
-    {
-      provide: APP_INITIALIZER,
-      useFactory: initializeAppFactory,
-      deps: [InitService],
-      multi: true,
-    },
   ],
 }).catch((err) => console.error(err));
