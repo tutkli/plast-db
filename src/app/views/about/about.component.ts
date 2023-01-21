b05b7c1e31b3977796b6c34cfeccf28cd0a659ad
Clara Castillo <claracastillochale@gmail.com>
2023-01-21 16:28:49 +0100

    refactor: about page

--- src/app/views/about/about.component.ts
+++ src/app/views/about/about.component.ts
@@ -1,12 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'plast-about',
   standalone: true,
-  imports: [CommonModule],
+  imports: [],
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AboutComponent {}
